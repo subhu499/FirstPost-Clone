7e9a1e603e2dfed05d2acfbcc0989a842f936d77
Laxmi Kushwaha <laxmikushwaha2801@gmail.com>
2022-09-30 19:12:10 +0530

    update entertainmet page

--- entertain.js
+++ entertain.js
@@ -1,32 +1,37 @@
 let api= `https://firstpost-masai-server.herokuapp.com/ENTERTAINMENT`
 
+let clickdata=[];
 let data=async ()=>{
     let res=await fetch(api);
     let data1=await res.json();
      appendnews(data1)
-     appendvideo(data1)
+    clickdata=data1;
+    
+
     console.log(data1)
 }
 data();
 
-
-let appendslider = () =>{
-    let main = document.createElement("div");
-    let div =document.createElement("div");
-    let img=document.createElement("img");
-    let p=document.createElement("p");
-    let heading=document.createElement("h2");
-
-
-
-
-
+let api2 =`https://firstpost-masai-server.herokuapp.com/HOME-PAGE`
+let mostread=async (api)=>{
+    let res=await fetch(api);
+    let data1=await res.json();
+     
+    //  appendvideo(data1)
+     sideappend(data1)
+     
+    console.log(data1)
 }
+mostread(api2);
+
 let c=0;
 let appendnews = (data)=>{
    let container=document.getElementById("container");
    container.innerHTML=null;
+   
 
+  
+  
 data.map(function(el){
 c++;
 if(c!=5){
@@ -47,6 +52,9 @@ if(c!=5){
         
         div2.append(p,heading,subhead)
         div.append(img,div2);
+        div.addEventListener("click", function(){
+            shownews(el)
+        });
         container.append(div)
 }
 else{
@@ -64,7 +72,7 @@ title:`I can’t write a book about any part of my life. I can’t even have a c
     for(let i=0; i<3; i++){
         let div = document.createElement("div");
         let img = document.createElement("img");
-        let title = document.createElement("h3");
+        let title = document.createElement("p");
         img.src=arr[i].image;
         title.innerText=arr[i].title;
         div.append(img,title);
@@ -74,37 +82,98 @@ title:`I can’t write a book about any part of my life. I can’t even have a c
 }
 }
     })
-    
-
-
-
-}
+    }
  
-let appendvideo=(data)=>{
-    let container=document.getElementById("videos");
-    container.innerHTML=null
+
+   
+let sideappend=(data)=>{
+    let container=document.getElementById("container1")
+    let counter=0
+
+
+    container.innerHTML=null;
+
     data.map(function(el){
-        let div=document.createElement('div');
-        let div1=document.createElement("div")
+        counter++
+        let head=document.createElement("h2")
+        head.innerText="MOST READ";
+        let div=document.createElement("div");
+        div.setAttribute("id","sidebar")
+        let div1=document.createElement("div");
+        let num=document.createElement("h2")
+        num.innerText=counter;
+        num.setAttribute("id","num1")
 
-        let v1=document.createElement("div")
-        let v2=document.createElement("div")
-        let v3=document.createElement("div")
-
-        let img1=document.createElement("img")
-        let img2=document.createElement("img")
-        let img3=document.createElement("img")
-
-        img1.src=el.image;
+        let div3=document.createElement("div");
+        div3.setAttribute("id","countshad");
         
+        let div2=document.createElement("div");
+        let h3=document.createElement("h4")
+        let p=document.createElement("p")
+        p.setAttribute("id","pp")
+        h3.innerText=el.title;
+        p.innerText=el.summary
 
 
+        div1.append(num,div3);
+        div2.append(h3,p)
 
 
+        
+        div.append(div1,div2);
+
+        div.addEventListener("click", function(){
+            shownews(el)
+        });
+        container.append(div)
 
 
+        
     })
 }
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 1000); // Change image every 2 seconds
+}
+let arr=[];
+function shownews(el){
+    arr.push(el);
+    localStorage.setItem("shownews",JSON.stringify(arr));
+    window.location.href="show.html";
+} 
+
+let myslides = document.querySelectorAll(".mySlides");
+setTimeout(function(){
+    console.log(clickdata)
+
+    for(let i=0; i<myslides.length; i++){
+        
+        myslides[i].innerHTML=`<div class="numbertext">${i+1} / 3</div>
+        <img src="${clickdata[i+5].image}" style="width:100%">
+        <div class="text">${clickdata[i+5].title}</div>`
+        myslides[i].addEventListener("click",function(){
+            shownews(clickdata[i+5]);
+        })
+    }
+},5000)
+
 
 
 
